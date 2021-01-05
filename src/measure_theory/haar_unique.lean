2490697166096db0f8ae4b4eadd7d33a039be2e0
Floris van Doorn <fpvdoorn@gmail.com>
2021-01-05 03:30:07 -0500

    great progress

--- src/measure_theory/haar_unique.lean
+++ src/measure_theory/haar_unique.lean
@@ -6,6 +6,71 @@ open_locale ennreal
 
 /- todo: make `regular` a type class? -/
 
+-- make measure.prod irreducible (and measure.pi)
+
+namespace set
+
+variables {α β γ δ : Type*} {s : set α} {t : set β}
+
+lemma prod_preimage_left {f : γ → α} : (f ⁻¹' s).prod t = (λp, (f p.1, p.2)) ⁻¹' (s.prod t) := rfl
+
+lemma prod_preimage_right {g : δ → β} : s.prod (g ⁻¹' t) = (λp, (p.1, g p.2)) ⁻¹' (s.prod t) := rfl
+
+lemma mk_preimage_prod_left_fn_eq_if {y : β} [decidable_pred (∈ t)] (f : γ → α) :
+  (λ x, (f x, y)) ⁻¹' s.prod t = if y ∈ t then f ⁻¹' s else ∅ :=
+by rw [← mk_preimage_prod_left_eq_if, prod_preimage_left, preimage_preimage]
+
+lemma mk_preimage_prod_right_fn_eq_if {x : α} [decidable_pred (∈ s)] (g : δ → β) :
+  (λ y, (x, g y)) ⁻¹' s.prod t = if x ∈ s then g ⁻¹' t else ∅ :=
+by rw [← mk_preimage_prod_right_eq_if, prod_preimage_right, preimage_preimage]
+
+lemma preimage_prod_mk_prod (f : γ → α) (g : γ → β) :
+  (λ x, (f x, g x)) ⁻¹' s.prod t = f ⁻¹' s ∩ g ⁻¹' t :=
+by { ext x, simp }
+
+
+end set
+open set
+
+namespace equiv
+
+variables {α β γ δ : Type*}
+/-- A variation on `equiv.prod_congr` where the equivalence in the second component can depend
+  on the first component. -/
+@[simps {fully_applied := ff}]
+def prod_twist {α₁ β₁ α₂ β₂ : Type*} (e₁ : α₁ ≃ α₂) (e₂ : α₁ → β₁ ≃ β₂) : α₁ × β₁ ≃ α₂ × β₂ :=
+{ to_fun := λ x : α₁ × β₁, (e₁ x.1, e₂ x.1 x.2),
+  inv_fun := λ y : α₂ × β₂, (e₁.symm y.1, (e₂ $ e₁.symm y.1).symm y.2),
+  left_inv := by { rintro ⟨x₁, y₁⟩, simp only [symm_apply_apply] },
+  right_inv := by { rintro ⟨x₁, y₁⟩, simp only [apply_symm_apply] } }
+
+end equiv
+
+section
+
+variables {G : Type*} [topological_space G] [group G] [topological_group G]
+
+variable (G)
+/-- The map `(x, y) ↦ (x, xy)` as a homeomorphism. -/
+@[to_additive prod_add_right "The map `(x, y) ↦ (x, x + y)` as a homeomorphism."]
+protected def homeomorph.prod_mul_right : G × G ≃ₜ G × G :=
+{ continuous_to_fun  := continuous_fst.prod_mk continuous_mul,
+  continuous_inv_fun := continuous_fst.prod_mk $ continuous_fst.inv.mul continuous_snd,
+  .. equiv.prod_twist (equiv.refl _) equiv.mul_left }
+
+@[simp, to_additive homeomorph.prod_add_right_coe]
+lemma homeomorph.prod_mul_right_coe :
+  ⇑(homeomorph.prod_mul_right G) = λ z : G × G, (z.1, z.1 * z.2) :=
+rfl
+
+@[simp, to_additive homeomorph.prod_add_right_symm_coe]
+lemma homeomorph.prod_mul_right_symm_coe :
+  ⇑(homeomorph.prod_mul_right G).symm = λ z : G × G, (z.1, z.1⁻¹ * z.2) :=
+rfl
+
+variable {G}
+end
+
 namespace topological_space
 
 variables (α : Type*) [topological_space α]
@@ -51,6 +116,25 @@ begin
     rw [mem_Union], exact ⟨n, hn⟩ }
 end
 
+/- CHOOSE ABOVE OR BELOW -/
+
+instance locally_compact_group.sigma_compact_space [locally_compact_space G] :
+  sigma_compact_space G :=
+begin
+  constructor,
+  obtain ⟨L, h1L, h2L, h3L⟩ := exists_compact_subset is_open_univ (mem_univ (1 : G)),
+  refine ⟨λ n, (λ x, x * dense_seq G n) ⁻¹' L, _, _⟩,
+  { intro n, exact (homeomorph.mul_right _).compact_preimage.mpr h1L },
+  { rw [eq_univ_iff_forall],
+    intro x,
+    obtain ⟨_, hn, ⟨n, rfl⟩⟩ : ((λ y, x * y) ⁻¹' L ∩ range (dense_seq G)).nonempty :=
+    (dense_iff_inter_open.mp (dense_range_dense_seq G) _
+      ((homeomorph.mul_left _).continuous.is_open_preimage _ is_open_interior)
+      ⟨x⁻¹, by simp [homeomorph.mul_left, h2L]⟩).mono
+      (inter_subset_inter_left _ $ preimage_mono $ interior_subset),
+    rw [mem_Union], exact ⟨n, hn⟩ }
+end
+
 end topological_space
 open topological_space
 
@@ -77,7 +161,53 @@ ennreal.measurable_div.comp $ hf.prod_mk hg
 
 end
 
+section
+
+/-- extra simp lemma that `dsimp` can use -/
+@[simp, to_additive]
+lemma mul_left_symm_apply {G} [group G] (a : G) : ((equiv.mul_left a).symm : G → G) = (*) a⁻¹ :=
+rfl
+
+/-- extra simp lemma that `dsimp` can use -/
+@[simp, to_additive]
+lemma mul_right_symm_apply {G} [group G] (a : G) :
+  ((equiv.mul_right a).symm : G → G) = λ x, x * a⁻¹ :=
+rfl
+
+end
+
+namespace measurable_equiv
+
+variables {α β : Type*} [measurable_space α] [measurable_space β]
+
+@[simp] theorem symm_comp_self (e : α ≃ᵐ β) : e.symm ∘ e = id := funext e.left_inv
+
+@[simp] theorem self_comp_symm (e : α ≃ᵐ β) : e ∘ e.symm = id := funext e.right_inv
+
+
+end measurable_equiv
+
+section
+
+variables {α : Type*} {β : Type*} [topological_space α]
+  [measurable_space α] [borel_space α] [topological_space β] [measurable_space β]
+  [borel_space β]
+
+set_option trace.simps.verbose true
+
+@[simp]
+lemma homeomorph.to_measurable_equiv_coe (h : α ≃ₜ β) : (h.to_measurable_equiv : α → β) = h :=
+rfl
+
+@[simp] lemma homeomorph.to_measurable_equiv_symm_coe (h : α ≃ₜ β) :
+  (h.to_measurable_equiv.symm : β → α) = h.symm :=
+rfl
+
+
+end
+
 namespace measure_theory
+
 
 namespace measure
 
@@ -96,30 +226,58 @@ lemma lintegral_prod_mul {f : α → ennreal} {g : β → ennreal} (hf : measura
 by simp [lintegral_prod _ ((hf.comp measurable_fst).ennreal_mul $ hg.comp measurable_snd),
   lintegral_lintegral_mul hf hg]
 
+lemma map_symm_map (e : α ≃ᵐ β) : map e.symm (map e μ) = μ :=
+by simp [map_map e.symm.measurable e.measurable]
+
+lemma map_map_symm (e : α ≃ᵐ β) : map e (map e.symm ν) = ν :=
+by simp [map_map e.measurable e.symm.measurable]
+
+lemma map_measurable_equiv_injective (e : α ≃ᵐ β) : injective (map e) :=
+by { intros μ₁ μ₂ hμ, apply_fun map e.symm at hμ, simpa [map_symm_map e] using hμ }
+
+lemma map_apply_eq_iff_map_symm_apply_eq (e : α ≃ᵐ β) : map e μ = ν ↔ map e.symm ν = μ :=
+by rw [← (map_measurable_equiv_injective e).eq_iff, map_map_symm, eq_comm]
+
 end prod
 
 variables {G : Type*} [group G] [topological_space G] [topological_group G]
-variables [measurable_space G] [borel_space G]
+variables [measurable_space G]
 variables {μ ν : measure G}
 
 section separable
 
+-- lemma regular.sigma_finite [opens_measurable_space G] [t2_space G] [sigma_compact_space G] (hμ : regular μ) : sigma_finite μ :=
+-- ⟨{ set := compact_covering G,
+--   set_mem := λ n, (is_compact_compact_covering G n).is_measurable,
+--   finite := λ n, hμ.lt_top_of_is_compact $ is_compact_compact_covering G n,
+--   spanning := Union_compact_covering G }⟩
+
+-- -- #print regular.sigma_finite
+-- instance (K₀ : positive_compacts G) [borel_space G] [t2_space G] [locally_compact_space G] [sigma_compact_space G] : sigma_finite (haar_measure K₀) :=
+-- regular_haar_measure.sigma_finite
+
+/- CHOOSE ABOVE OR BELOW -/
+
 variables [t2_space G] [separable_space G] [locally_compact_space G]
 
 /-- If we define σ-compact spaces, we could easily prove this for those. -/
-lemma regular.sigma_finite (hμ : regular μ) : sigma_finite μ :=
+lemma regular.sigma_finite [opens_measurable_space G] (hμ : regular μ) : sigma_finite μ :=
 let ⟨K, h1K, h2K⟩ := exists_compact_covering G in
 ⟨{ set := K,
   set_mem := λ n, (h1K n).is_measurable,
   finite := λ n, hμ.lt_top_of_is_compact $ h1K n,
   spanning := h2K }⟩
 
-instance (K₀ : positive_compacts G) : sigma_finite (haar_measure K₀) :=
+
+
+
+-- #print regular.sigma_finite
+instance (K₀ : positive_compacts G) [borel_space G] : sigma_finite (haar_measure K₀) :=
 regular_haar_measure.sigma_finite
 
 end separable
 
-variables [second_countable_topology G]
+variables [second_countable_topology G] [borel_space G]
 
 lemma is_left_invariant_lintegral (hμ : is_left_invariant μ) {f : G → ennreal} (hf : measurable f)
   (y : G) : ∫⁻ x, f (y * x) ∂μ = ∫⁻ x, f x ∂μ :=
@@ -133,53 +291,151 @@ variables [t2_space G]
 
 variables [sigma_finite ν] [sigma_finite μ]
 
-lemma measure_inv_ne_zero (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E) (h2E : μ E ≠ 0) :
-  μ ((λ x, x⁻¹) ⁻¹' E) ≠ 0 :=
+
+/-- This condition is part of the definition of a measurable group in [Halmos]. -/
+lemma map_prod_mul_eq (hμ : is_left_invariant μ) (hν : is_left_invariant ν) :
+  map (λ z : G × G, (z.1, z.1 * z.2)) (μ.prod ν) = μ.prod ν :=
 begin
+  refine (prod_eq _).symm, intros s t hs ht,
+  simp_rw [map_apply (measurable_fst.prod_mk (measurable_fst.mul measurable_snd)) (hs.prod ht),
+    prod_apply ((measurable_fst.prod_mk (measurable_fst.mul measurable_snd)) (hs.prod ht)),
+    preimage_preimage],
+  conv_lhs { congr, skip, funext, rw [mk_preimage_prod_right_fn_eq_if ((*) x), measure_if] },
+  simp_rw [hν _ ht, lintegral_indicator _ hs, lintegral_const, restrict_apply_univ, mul_comm]
+end
+
+lemma map_prod_mul_eq_swap (hμ : is_left_invariant μ) (hν : is_left_invariant ν) :
+  map (λ z : G × G, (z.2, z.2 * z.1)) (μ.prod ν) = ν.prod μ :=
+begin
+  rw [← prod_swap],
+  simp_rw [map_map (measurable_snd.prod_mk (measurable_snd.mul measurable_fst)) measurable_swap],
+  exact map_prod_mul_eq hν hμ
+end
+
+lemma map_prod_inv_mul_eq (hμ : is_left_invariant μ) (hν : is_left_invariant ν) :
+  map (λ z : G × G, (z.1, z.1⁻¹ * z.2)) (μ.prod ν) = μ.prod ν :=
+(map_apply_eq_iff_map_symm_apply_eq $ (homeomorph.prod_mul_right G).to_measurable_equiv).mp $
+  map_prod_mul_eq hμ hν
+
+lemma map_prod_inv_mul_eq_swap (hμ : is_left_invariant μ) (hν : is_left_invariant ν) :
+  map (λ z : G × G, (z.2, z.2⁻¹ * z.1)) (μ.prod ν) = ν.prod μ :=
+begin
+  rw [← prod_swap],
+  simp_rw [map_map (measurable_snd.prod_mk (measurable_snd.inv.mul measurable_fst)) measurable_swap],
+  exact map_prod_inv_mul_eq hν hμ
+end
+
+lemma map_prod_mul_inv_eq (hμ : is_left_invariant μ) (hν : is_left_invariant ν) :
+  map (λ z : G × G, (z.2 * z.1, z.1⁻¹)) (μ.prod ν) = μ.prod ν :=
+begin
+  let S := (homeomorph.prod_mul_right G).to_measurable_equiv,
+  suffices : map ((λ z : G × G, (z.2, z.2⁻¹ * z.1)) ∘ (λ z : G × G, (z.2, z.2 * z.1))) (μ.prod ν) =
+    μ.prod ν,
+  { convert this, ext1 ⟨x, y⟩, simp },
+  simp_rw [← map_map (measurable_snd.prod_mk (measurable_snd.inv.mul measurable_fst))
+    (measurable_snd.prod_mk (measurable_snd.mul measurable_fst)), map_prod_mul_eq_swap hμ hν,
+    map_prod_inv_mul_eq_swap hν hμ]
+end
+
+lemma measure_null_of_measure_inv_null (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E)
+  (h2E : μ ((λ x, x⁻¹) ⁻¹' E) = 0) : μ E = 0 :=
+begin
+  have hf : measurable (λ z : G × G, (z.2 * z.1, z.1⁻¹)) :=
+  ((measurable_snd.mul measurable_fst).prod_mk measurable_fst.inv),
+  suffices : map (λ z : G × G, (z.2 * z.1, z.1⁻¹)) (μ.prod μ) (E.prod E) = 0,
+  { simpa only [map_prod_mul_inv_eq hμ hμ, prod_prod hE hE, mul_eq_zero, or_self] using this },
+  simp_rw [map_apply hf (hE.prod hE), prod_apply_symm (hf (hE.prod hE)), preimage_preimage,
+    preimage_prod_mk_prod],
+  convert lintegral_zero, ext1 x, refine measure_mono_null (inter_subset_right _ _) h2E
+end
+
+lemma measure_inv_null (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E) :
+  μ ((λ x, x⁻¹) ⁻¹' E) = 0 ↔ μ E = 0 :=
+begin
+  refine ⟨measure_null_of_measure_inv_null hμ hE, _⟩,
+  intro h2E, apply measure_null_of_measure_inv_null hμ (measurable_inv hE), convert h2E using 2,
+  exact set.inv_inv
+end
+
+lemma measure_eq_top_of_measure_inv_eq_top (hμ : is_left_invariant μ) {E : set G}
+  (hE : is_measurable E) (h2E : μ ((λ x, x⁻¹) ⁻¹' E) = ⊤) : μ E = ⊤ :=
+begin
+  -- have hf : measurable (λ z : G × G, (z.2 * z.1, z.1⁻¹)) :=
+  -- ((measurable_snd.mul measurable_fst).prod_mk measurable_fst.inv),
+  -- suffices : map (λ z : G × G, (z.2 * z.1, z.1⁻¹)) (μ.prod μ) (E.prod E) = ⊤,
+  -- { simpa only [map_prod_mul_inv_eq hμ hμ, prod_prod hE hE, mul_eq_zero, or_self] using this },
+  -- simp_rw [map_apply hf (hE.prod hE), prod_apply_symm (hf (hE.prod hE)), preimage_preimage,
+  --   preimage_prod_mk_prod],
+  -- convert lintegral_zero, ext1 x, refine measure_mono_null (inter_subset_right _ _) h2E
   sorry
 end
 
-lemma measure_inv_ne_top (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E) (h2E : μ E ≠ ⊤) :
-  μ ((λ x, x⁻¹) ⁻¹' E) ≠ ⊤ :=
+lemma measure_inv_eq_top (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E) :
+  μ ((λ x, x⁻¹) ⁻¹' E) = ⊤ ↔ μ E = ⊤ :=
 begin
-  sorry
+  refine ⟨measure_eq_top_of_measure_inv_eq_top hμ hE, _⟩,
+  intro h2E, apply measure_eq_top_of_measure_inv_eq_top hμ (measurable_inv hE), convert h2E using 2,
+  exact set.inv_inv
 end
 
-lemma measure_mul_right_ne_zero (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E) (h2E : μ E ≠ 0) (y : G) :
-  μ ((λ x, x * y) ⁻¹' E) ≠ 0 :=
+-- lemma measure_inv_ne_zero (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E) (h2E : μ E ≠ 0) :
+--   μ ((λ x, x⁻¹) ⁻¹' E) ≠ 0 :=
+-- begin
+--   have hf : measurable (λ z : G × G, (z.2 * z.1, z.1⁻¹)) :=
+--   ((measurable_snd.mul measurable_fst).prod_mk measurable_fst.inv),
+--   have : map (λ z : G × G, (z.2 * z.1, z.1⁻¹)) (μ.prod μ) (E.prod E) ≠ 0,
+--   { simp_rw [map_prod_mul_inv_eq hμ hμ, prod_prod hE hE, ← zero_lt_iff_ne_zero, ennreal.mul_pos,
+--       and_self, zero_lt_iff_ne_zero], exact h2E },
+--   refine mt (λ h, _) this,
+--   simp_rw [map_apply hf (hE.prod hE), prod_apply_symm (hf (hE.prod hE)), preimage_preimage,
+--     preimage_prod_mk_prod],
+--   convert lintegral_zero, ext1 x, refine measure_mono_null (inter_subset_right _ _) h
+-- end
+
+lemma measurable_measure_mul_right (hμ : is_left_invariant μ) {E : set G}
+  (hE : is_measurable E) : measurable (λ x, μ ((λ y, y * x) ⁻¹' E)) :=
 begin
-  sorry
+  suffices :
+    measurable (λ y, μ ((λ x, (x, y)) ⁻¹' ((λ z : G × G, (1, z.1 * z.2)) ⁻¹' set.prod univ E))),
+  { convert this, ext1 x, congr' 1 with y : 1, simp },
+  apply measurable_measure_prod_mk_right,
+  exact measurable_const.prod_mk (measurable_fst.mul measurable_snd) (is_measurable.univ.prod hE)
 end
 
-lemma measure_mul_right_ne_top (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E) (h2E : μ E ≠ ⊤) (y : G) :
-  μ ((λ x, x * y) ⁻¹' E) ≠ ⊤ :=
-begin
-  sorry
-end
-
-lemma measurable_measure_mul_right (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E) :
-  measurable (λ x, μ ((λ y, y * x) ⁻¹' E)) :=
-begin
-  sorry
-end
-
-lemma lintegral_lintegral_mul_inv (hμ : is_left_invariant μ) (hν : is_left_invariant ν) (f : G → G → ennreal) (hf : measurable (uncurry f)) :
+lemma lintegral_lintegral_mul_inv (hμ : is_left_invariant μ) (hν : is_left_invariant ν)
+  (f : G → G → ennreal) (hf : measurable (uncurry f)) :
   ∫⁻ x, ∫⁻ y, f (y * x) x⁻¹ ∂ν ∂μ = ∫⁻ x, ∫⁻ y, f x y ∂ν ∂μ :=
 begin
-  symmetry,
-  conv_lhs { congr, skip, funext, rw [← is_left_invariant_lintegral hν hf.of_uncurry_left x⁻¹] },
-  simp_rw [lintegral_lintegral_swap (show measurable (uncurry (λ (x y : G), f x (x⁻¹ * y))), from
-    hf.comp (measurable_fst.prod_mk $ measurable_fst.inv.mul measurable_snd))],
-  -- the following double-show is a bit ugly, but the `rw` cares a lot about the formulation of the
-  -- argument, and the elaborator has trouble when given the first type.
-  conv_lhs { congr, skip, funext,
-    rw [← is_left_invariant_lintegral hμ (show measurable (λ x, f x (x⁻¹ * y)), from
-      show measurable (uncurry f ∘ λ (x : G), (x, x⁻¹ * y)), from
-      hf.comp (measurable_id.prod_mk $ measurable_inv.mul measurable_const)) y] },
-  simp_rw [mul_inv_rev, inv_mul_cancel_right],
-  rw [lintegral_lintegral_swap],
-  exact hf.comp ((measurable_fst.mul measurable_snd).prod_mk measurable_snd.inv)
+  have h2f : measurable (uncurry $ λ x y, f (y * x) x⁻¹) :=
+  hf.comp ((measurable_snd.mul measurable_fst).prod_mk measurable_fst.inv),
+  simp_rw [lintegral_lintegral h2f, lintegral_lintegral hf],
+  conv_rhs { rw [← map_prod_mul_inv_eq hμ hν] },
+  symmetry, exact lintegral_map hf ((measurable_snd.mul measurable_fst).prod_mk measurable_fst.inv)
 end
+
+lemma measure_mul_right_null (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E) (y : G) :
+  μ ((λ x, x * y) ⁻¹' E) = 0 ↔ μ E = 0 :=
+begin
+  rw [← measure_inv_null hμ hE, ← hμ y⁻¹ (measurable_inv hE),
+    ← measure_inv_null hμ (measurable_mul_right y hE)],
+  convert iff.rfl using 3, ext x, simp,
+end
+
+lemma measure_mul_right_eq_top (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E)
+  (y : G) : μ ((λ x, x * y) ⁻¹' E) = ⊤ ↔ μ E = ⊤ :=
+begin
+  rw [← measure_inv_eq_top hμ hE, ← hμ y⁻¹ (measurable_inv hE),
+    ← measure_inv_eq_top hμ (measurable_mul_right y hE)],
+  convert iff.rfl using 3, ext x, simp,
+end
+
+lemma measure_mul_right_ne_zero (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E)
+  (h2E : μ E ≠ 0) (y : G) : μ ((λ x, x * y) ⁻¹' E) ≠ 0 :=
+(not_iff_not_of_iff (measure_mul_right_null hμ hE y)).mpr h2E
+
+lemma measure_mul_right_ne_top (hμ : is_left_invariant μ) {E : set G} (hE : is_measurable E)
+  (h2E : μ E ≠ ⊤) (y : G) : μ ((λ x, x * y) ⁻¹' E) ≠ ⊤ :=
+(not_iff_not_of_iff (measure_mul_right_eq_top hμ hE y)).mpr h2E
 
 /-! We follow the book Measure Theory by Donald Cohn. -/
 
